be724c781878e39ee7284afbc59a9fc27195f336
Kvadrokom <lortrem@mail.ru>
2023-10-26 20:28:56 +0300

    add jenkinsfile

---
+++ jenkinsfile_1.groovy
@@ -0,0 +1,27 @@
+pipeline {
+    agent any
+    stages {
+        stage('example') {
+            steps {
+                echo 'Hello world!'
+                script {
+                    def browsers = ['chrome', 'firefox']
+                    for (int i = 0; i < browsers.size(); ++i)
+                        echo "testing the ${ 'browsers' } browser"
+                }
+            }
+        }
+    }
+    post {
+        failure{
+            echo 'I will always say Hello only failure'
+        }
+        success {
+            echo 'I will always say Hello only success'
+        }
+        always {
+            echo 'I will always say Hello only again'
+            cleanWs()
+        }
+    }
+}
